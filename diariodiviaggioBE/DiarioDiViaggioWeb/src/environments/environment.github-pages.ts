97814e60df8446d51d74739cba9dd7420bf97a06
Ale <alessandro.clima@outlook.it>
2025-07-27 11:02:58 +0200

    rollback

--- diariodiviaggioBE/DiarioDiViaggioWeb/src/environments/environment.github-pages.ts
+++ diariodiviaggioBE/DiarioDiViaggioWeb/src/environments/environment.github-pages.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lericettedialeinborghese-gdfvh7ccgxc9a0e8.italynorth-01.azurewebsites.net/api' // Your backend API URL without /api
+  apiUrl: 'https://lericettedialeinborghese-gdfvh7ccgxc9a0e8.italynorth-01.azurewebsites.net' // Your backend API URL without /api
 };
